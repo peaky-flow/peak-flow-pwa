c0b955fe81466503d8dd114b7937f9aefba160e0
fredkingham <fredkingham@gmail.com>
2020-03-03 17:11:21 +0000

    Adds bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
