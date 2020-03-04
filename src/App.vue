c0243bce28b661c3e16bf7f227bbaf4b595bd530
fredkingham <fredkingham@gmail.com>
2020-03-04 21:01:53 +0000

    Adds font awesome and a basic colour scheme

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,17 @@
   <div class="container-fluid" id="app">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
 
-    <nav class="navbar fixed-bottom navbar-light bg-light">
-        <a class="navbar-brand" href="#">Fixed bottom</a>
+    <nav class="peak-nav-bar">
+        <a class="peak-menu-item">
+            <i class="pencil"></i>
+            <font-awesome-icon icon="pencil-alt" />
+        </a>
+        <a class="peak-menu-item">
+            <font-awesome-icon icon="list" />
+        </a>
+        <a class="peak-menu-item">
+            <font-awesome-icon icon="envelope" />
+        </a>
     </nav>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPencilAlt, faEnvelope, faList } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPencilAlt)
+library.add(faEnvelope)
+library.add(faList)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 
